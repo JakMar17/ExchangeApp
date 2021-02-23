0f5c141cfa4c7f056a78d4a5c8657e483640f0fd
jakmar17 <jakob.marusic17@gmail.com>
2021-02-23 21:20:15 +0100

    added static site generator

---
+++ backend-static-sites/src/main/java/si/fri/jakmar/backend/sites/exchangeappstaticsites/ExchangeAppStaticSitesApplication.java
@@ -0,0 +1,13 @@
+package si.fri.jakmar.backend.sites.exchangeappstaticsites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeAppStaticSitesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeAppStaticSitesApplication.class, args);
+    }
+
+}
---
+++ backend-static-sites/src/test/java/si/fri/jakmar/backend/sites/exchangeappstaticsites/ExchangeAppStaticSitesApplicationTests.java
@@ -0,0 +1,13 @@
+package si.fri.jakmar.backend.sites.exchangeappstaticsites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeAppStaticSitesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
